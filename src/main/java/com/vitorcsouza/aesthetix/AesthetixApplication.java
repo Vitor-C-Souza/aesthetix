57af8c2280fadf3e4e67c1211201c539b0fbd74c
Vítor Cavalcante Souza <51930276+Vitor-C-Souza@users.noreply.github.com>
2026-08-11 15:08:38 -0300

    Initialize Aesthetix application with Maven setup and basic structure

---
+++ src/main/java/com/vitorcsouza/aesthetix/AesthetixApplication.java
@@ -0,0 +1,13 @@
+package com.vitorcsouza.aesthetix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesthetixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AesthetixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitorcsouza/aesthetix/AesthetixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorcsouza.aesthetix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AesthetixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

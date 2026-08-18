20427b02daa81313113731169e47f30dced15cc0
Vítor Cavalcante Souza <51930276+Vitor-C-Souza@users.noreply.github.com>
2026-08-18 19:07:25 -0300

    Refactor application properties for environment variable support; remove TokenPort interface

--- src/main/java/com/vitorcsouza/aesthetix/domain/port/out/TokenPort.java
+++
@@ -1,9 +0,0 @@
-package com.vitorcsouza.aesthetix.domain.port.out;
-
-import com.vitorcsouza.aesthetix.domain.model.Role;
-
-import java.util.Set;
-
-public interface TokenPort {
-    String generateToken(String username, Set<Role> roles);
-}
